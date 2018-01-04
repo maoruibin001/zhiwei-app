91010b07895a924328a2b9a316345780774300d5
maoruibin001 <www.maoruibin@163.com>
2018-01-04 09:38:37 +0800

    新增文件提交

---
+++ src/components/info/Info.js
@@ -0,0 +1,19 @@
+/**
+ * Created by lenovo on 2017/12/27.
+ */
+import React, {Component} from 'react';
+console.log(333)
+import {
+  View,
+  Text,
+} from 'react-native';
+
+class Info extends Component {
+  render() {
+    return <View>
+      <Text>hello i am Info22ksdfsafdasfdasdf</Text>
+    </View>
+  }
+}
+
+export default Info;
